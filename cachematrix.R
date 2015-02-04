531089c4bd47865c48b0eba262931445139eff66
unknown <jgruber@jgruber-desktop.(none)>
2015-02-04 23:04:06 +0100

    implemented assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## -------------------------------------------------------------
+## Programming assignment 2: Caching the Inverse of a Matrix
+##
+## This program contains 2 functions and applies the given example
+## to the inversion of matrices
+## -------------------------------------------------------------
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function creates a list with functions to access the caching key and
+## the cached value. The function operates on a given matrix mtx.
+makeCacheMatrix <- function(mtx = matrix()) {
+  cachedValue <- NULL
+  list(set = function(newMtx) {mtx <<- newMtx; cachedValue <<- NULL}, 
+       get = function() {mtx},
+       setsolve = function(solve) {cachedValue <<- solve},
+       getsolve = function(){cachedValue}
+  )
 }
 
 
-## Write a short comment describing this function
-
+## This function retrieves a list-object, which was necessarily created by the
+## function makeCacheMatrix and returns the result of the matrix inversion. The
+## matrix inversion itself is executed only once.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  result <- x$getsolve()
+  if(!is.null(result)) {
+    message("getting cached data")
+    return(result)
+  }
+  result <- solve(x$get(), ...)
+  x$setsolve(result)
+  result
 }
